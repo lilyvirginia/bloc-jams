b39f2ae6763edece15fc35637378adcff91fa099
Lily Virginia <lilyvirginiamusic@gmail.com>
2017-10-24 13:33:35 -0400

    Add mobile responsive and playerbar

--- scripts/album.js
+++ scripts/album.js
@@ -78,15 +78,16 @@
     }
 };
 
-var findParentByClassName = function(element, targetClass){
-   if (element) {
-      var currentParent = element.parentElement;
-      while (currentParent.className !== targetClass && currentParent.className !== null){
-         currentParent = currentParent.parentElement;
-      }
-     return currentParent;
-   }
+var findParentByClassName = function(element, targetClass) {
+    if (element) {
+        var currentParent = element.parentElement;
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        }
+        return currentParent;
+    }
 };
+
 
 var getSongItem = function(element) {
     switch (element.className) {
