76d520c12a80de56060f7d0655c0134f311e409a
Lily Virginia <lilyvirginiamusic@gmail.com>
2017-10-25 10:57:02 -0400

    Add JQuery to landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,22 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
+     var revealPoint = function() {
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+     };
+     $.each($('.point'), revealPoint);
+   };
 
-var revealPoint = function(point) {
-   point.style.opacity = 1;
-   point.style.transform = "scaleX(1) translateY(0)";
-   point.style.msTransform = "scaleX(1) translateY(0)";
-   point.style.WebkitTransform = "scaleX(1) translateY(0)";
- }
-
-var animatePoints = function(points) {
-    forEach(points, revealPoint);
-};
-
-window.onload = function() {
-   if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {
+  if ($(window).height() > 950) {
+       animatePoints();
      }
+   var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-   var sellingPoints = document.getElementsByClassName('selling-points')[0];
-
-   window.addEventListener('scroll', function(event) {
-     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
+   $(window).scroll(function(event) {
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
         }
+   });
  });
-}
